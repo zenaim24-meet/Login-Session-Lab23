d62a4711894c208dad4ba219e7b89069f16bb285
zenaim24-meet <139694487+zenaim24-meet@users.noreply.github.com>
2023-07-20 18:41:09 +0300

    login

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,9 +4,20 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods= ['GET', 'POST'] ) # What methods are needed?
 def home():
-	
+	if request.method == 'POST':
+		try:
+			quote = request.form['quote']
+			name = request.form['name']
+			age = request.form['age']
+			login_session['quote'] = quote
+			login_session['name'] = name
+			login_session['age'] = age
+			return redirect(url_for('thanks'))
+		except:
+			return redirect(url_for('error'))
+
 	return render_template('home.html')
 
 
@@ -22,7 +33,7 @@ def display():
 	return render_template('display.html', ) # What variables are needed?
 
 
-@app.route('/thanks')
+@app.route('/thanks', methods= ['GET', 'POST'])
 def thanks():
 
 	return render_template('thanks.html')
